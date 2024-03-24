965143aa969df80b26fb615be0b2bdc3b9a2c2b9
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2024-03-24 20:34:46 +0530

    Test 01

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def create_dataFrame(file):
-    print('create_dataFrame Called')
-    df = pd.read_csv(file)
-    new_df = df.dropna()
-    return new_df
-
-def display_data():
-    print('display_data Called')
-    df = create_dataFrame(df)
-    return df.head(5)
-
-def set_date_column(df, date):
-    print('set_date_column Called')
-    df.loc[:,f'{date}'] = pd.to_datetime(df[f'{date}'])
-
-def set_analysis_field(df, field):
-    print('set_analysis_field Called')
-    sold_models = df[f'{field}'].value_counts()
-    return sold_models
-
 def plot_graph(df, time, field, graph_type):
     df[f'{time}'] = pd.to_datetime(df[f'{time}'], format='%Y')
     model_per_year = df.groupby([f'{time}', f'{field}']).size()
--- pages/Add.py
+++ pages/Add.py
@@ -1,16 +1,17 @@
 import streamlit as st
-from app import plot_graph, set_date_column, set_analysis_field, create_dataFrame
+from data import data_handler
+from app import plot_graph
 
 # Cache the create_dataFrame function
 @st.cache_data
 def cached_create_dataFrame(file):
-    return create_dataFrame(file)
+    return data_handler.create_dataFrame(file)
 
 def add():
     st.subheader("Import the Dataset")
     file = st.file_uploader('Upload CSV file', type=['csv'])
     if file is not None:
-        df = cached_create_dataFrame(file)  # Use cached function
+        df = data_handler.create_dataFrame(file)  # Use data_handler to create DataFrame
         st.success('File Uploaded Successfully')
         st.dataframe(df.head())
         return df
@@ -30,8 +31,8 @@ def plot():
         # Button to plot the graph
         if st.button("Plot Graph"):
             if time_in and field_in:  # Check if both time and field inputs are provided
-                time = set_date_column(df, time_in)
-                field = set_analysis_field(df, field_in)
+                time = data_handler.set_date_column(df, time_in)
+                field = data_handler.set_analysis_field(df, field_in)
                 plot_graph(df, time, field, graph_type)
             else:
                 st.warning("Please enter values for 'Time' and 'Field' to plot the graph.")
