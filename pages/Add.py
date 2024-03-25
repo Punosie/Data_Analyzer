df8b15aa4366b3d5e25ce39c62219c891eda8b99
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2024-03-26 03:13:04 +0530

    Revert "Test 01"
    This reverts commit 965143aa969df80b26fb615be0b2bdc3b9a2c2b9.

--- pages/Add.py
+++ pages/Add.py
@@ -1,17 +1,16 @@
 import streamlit as st
-from data import data_handler
-from app import plot_graph
+from app import plot_graph, set_date_column, set_analysis_field, create_dataFrame
 
 # Cache the create_dataFrame function
 @st.cache_data
 def cached_create_dataFrame(file):
-    return data_handler.create_dataFrame(file)
+    return create_dataFrame(file)
 
 def add():
     st.subheader("Import the Dataset")
     file = st.file_uploader('Upload CSV file', type=['csv'])
     if file is not None:
-        df = data_handler.create_dataFrame(file)  # Use data_handler to create DataFrame
+        df = cached_create_dataFrame(file)  # Use cached function
         st.success('File Uploaded Successfully')
         st.dataframe(df.head())
         return df
@@ -31,8 +30,8 @@ def plot():
         # Button to plot the graph
         if st.button("Plot Graph"):
             if time_in and field_in:  # Check if both time and field inputs are provided
-                time = data_handler.set_date_column(df, time_in)
-                field = data_handler.set_analysis_field(df, field_in)
+                time = set_date_column(df, time_in)
+                field = set_analysis_field(df, field_in)
                 plot_graph(df, time, field, graph_type)
             else:
                 st.warning("Please enter values for 'Time' and 'Field' to plot the graph.")
